5db3626f3e41382f5c12e9928511e02a793f3a91
sonseongyeol <sonseong10@gmail.com>
2023-08-12 21:28:39 +0900

    🔧 Update config setting

--- next.config.js
+++ next.config.js
@@ -1,18 +1,28 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
+const path = require('path');
 
 const nextConfig = {
+  swcMinify: true, // babel대신 swc사용
+  reactStrictMode: false,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:slug.xml',
+        destination: '/[slug].xml',
+      },
+    ];
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
