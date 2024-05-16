95fd6f783fb845ab58bb5e87a7e1ddef29222046
sonseongyeol <sonseong10@gmail.com>
2024-05-16 16:02:15 +0900

    placeholder 명칭 변경

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -10,10 +10,7 @@ function Header(): JSX.Element {
   return (
     <header className={styles.header}>
       <div className={styles.div}>
-        <InputText
-          inputType="search"
-          placeholder="당신이 찾고싶은 포케몬 이름을 입력해 주세요. 피카츄, 라이츄, 파이리, 꼬부기,"
-        />
+        <InputText inputType="search" placeholder="포케몬 이름을 입력해 주세요.." />
       </div>
     </header>
   );
